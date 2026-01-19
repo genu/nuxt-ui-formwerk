62383656a524589cf02420e46c700d06ad7c2cb0
Eugen Istoc <eugenistoc@gmail.com>
2026-01-19 14:24:54 -0500

    fix: remove @nuxt/ui module from configuration since we already have it as a module dependency

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-12-15",
-  modules: ["../src/module", "@nuxt/ui"],
+  modules: ["../src/module"],
   css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
 })
